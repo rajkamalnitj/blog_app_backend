4c3e9b74449061e8557aef1f83da7edf4bc94977
rajkamalnitj <rajkamalk.cs.19@nitj.ac.in>
2022-02-05 07:44:09 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
- const express=require("express");
+const express = require('express');
 
-const app=express();
+const app = express();
+const dotenv = require('dotenv');
+
+const mongoose = require('mongoose');
+
+dotenv.config();
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 var http = require('http');
+app.use('/', (req, res) => {
+  console.log('hey this is /raj url');
+});
 
+console.log(process.env.MONGO_URL);
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log('connected to mongodb'))
+  .catch((err) => console.log(err));
 
-
- app.listen("5050",()=>{
-
-console.log("backend is running");
-
- })
+app.listen('5050', () => {
+  console.log('backend is running');
+});
